56f76e791e4456c5791463f55602ec791feff8d9
martyanov.vladimir <martyanov.vladimir@uep24.ru>
2023-03-06 10:05:14 +0300

    add page.html

--- html/js/script.js
+++ html/js/script.js
@@ -1,7 +1,4 @@
 $(document).ready(function() {
-    $('.menu-burger__header').click(function() {
-        $('.mobile').toggleClass('active');
-    });
     $('.mobile-burger').click(function() {
         $('.mobile').toggleClass('active');
     });
